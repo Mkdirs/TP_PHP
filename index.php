f4cdb808649718992e64874ae2ec4d873c0cc283
Hafez Ben-Abdillah MADI <hafezbenabdillah.madi@gmail.com>
2023-12-18 13:19:28 +0100

    YOLO

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+
+<body>
+
+<h1>S01E01</h1>
+<a title="Tableau partie 1" href="/common/S01E01/Array/main.php">Tableau partie 1</a>
+<br>
+<a title="Fichier partie 1" href="/common/S01E01/File/main.php">Fichier partie 1</a>
+<br>
+<a title="Opérateur partie 1" href="/common/S01E01/Operators/main.php">Opérateur partie 1</a>
+<br>
+<a title="Constructeur partie 1" href="/common/S01E01/Construct/main.php">Opérateur partie 1</a>
+<br>
+<a title="Classe partie 1" href="/common/S01E01/Class/Initial.php">Classe partie 1</a>
+<br>
+<h1>S01E02</h1>
+<a title="class partie 1" href="/common/S01E02/main.php">class partie 1</a>
+<br>
+
+
+<br>
+<h1>S01E03</h1>
+<a title="Partie 3" href="/common/S01E03/s01e03.php">Partie 3</a>
+<br>
+
+<br>
+<h1>S01E04-5</h1>
+<br>
+
+
+</body>
+</html>
